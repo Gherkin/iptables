d27eb4ce6e8cab70cd844e60fa14d198bedd5fca
Gherkin <niyohn@stekligan.se>
2015-10-23 21:05:22 +0200

    added rule

--- iptables.sh
+++ iptables.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+SERVERIP=
+DNSIP=
+
 
 # Accept everything on loopback
 -A INPUT -i lo -j ACCEPT
 
 # Accept ssh connections from everywhere
--A INPUT -d 178.73.194.168/32 -p tcp -m tcp --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+-A INPUT -d $SERVERIP/32 -p tcp -m tcp --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 # Accept everything that is a response to a package we sent
 -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
@@ -22,10 +25,10 @@
 -A OUTPUT -o lo -j ACCEPT
 
 # Accept output going to ssh
--A OUTPUT -s 178.73.194.168/32 -p tcp -m tcp --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
+-A OUTPUT -s $SERVERIP/32 -p tcp -m tcp --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
 
 # Accept connections to the DNS
--A OUTPUT -s 178.73.194.168/32 -d 80.67.0.2/32 -o enp3s0 -p udp -m udp --dport 53 -m state --state NEW -j ACCEPT
+-A OUTPUT -s $SERVERIP/32 -d $DNSIP/32 -o enp3s0 -p udp -m udp --dport 53 -m state --state NEW -j ACCEPT
 
 # Accept everything going to the virtual machines
 -A OUTPUT -o virbr0 -j ACCEPT
